1478f857bdde08faf8dd9121eec45bcd6787b297
Neha Mittal <neha.mittal@airtel.com>
2024-08-31 21:49:37 +0530

    Integrate with Eureka Client Discovery

--- src/main/java/org/jboss/eap/quickstarts/kitchensink/KitchensinkApplication.java
+++ src/main/java/org/jboss/eap/quickstarts/kitchensink/KitchensinkApplication.java
@@ -3,13 +3,14 @@ package org.jboss.eap.quickstarts.kitchensink;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
 @EnableConfigurationProperties
 @ComponentScan(value = {"org.jboss.eap.quickstarts.kitchensink.*"})
-
+@EnableDiscoveryClient
 public class KitchensinkApplication {
 
 	public static void main(String[] args) {
